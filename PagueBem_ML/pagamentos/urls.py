139acebf876f6d46aa707d03878ea01cf22e3ec6
GustavoFelips <130930887+GustavoFelips@users.noreply.github.com>
2024-10-29 16:01:46 -0300

    Url Credor

--- PagueBem_ML/pagamentos/urls.py
+++ PagueBem_ML/pagamentos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import devedor_list, devedor_detail
+from .views import devedor_list, devedor_detail, credor_view
 
 urlpatterns = [
     path('devedores/', devedor_list, name='devedor_list'),
     path('devedores/<int:pk>/', devedor_detail, name='devedor_detail'),
+    path('credores/', credor_view, name='credor_view'),
 ]
