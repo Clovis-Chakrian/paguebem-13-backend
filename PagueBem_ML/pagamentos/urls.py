7b2ae069e5ec1b5476ec5dc83b99f00910419b16
AC <acsimoes446@gmail.com>
2024-10-29 20:05:04 -0300

    feat: add metodos do crud de exemplo

--- PagueBem_ML/pagamentos/urls.py
+++ PagueBem_ML/pagamentos/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import devedor_list, devedor_detail
+from .views import ExampleDetail, ExampleList, devedor_detail, devedor_list
 from . import views
 
 urlpatterns = [
     path('devedores/', devedor_list, name='devedor_list'),
     path('devedores/<int:pk>/', devedor_detail, name='devedor_detail'),
-    path('exemplo/', views.devedor_list_2),
-
+    path('exemplo/', ExampleList.as_view(), name='exemplo-list'),
+    path('exemplo/<int:devedor_id>/', ExampleDetail.as_view(), name='exemplo-detail'),
 ]
--- PagueBem_ML/pagamentos/views.py
+++ PagueBem_ML/pagamentos/views.py
@@ -46,19 +46,53 @@ def devedor_detail(request, pk):
         devedor.delete()
         return JsonResponse({'message': 'Devedor deletado.'})
 
+##Métodos de exemplo usando as conveções
+# Classe para listar e criar novos devedores
+class ExampleList(generics.ListCreateAPIView):
+    queryset = Devedor.objects.all()
+    serializer_class = DevedorCadastroSerialzer
 
-@csrf_exempt  # Para desativar a proteção CSRF (não recomendado em produção)
-@swagger_auto_schema(method='POST', request_body=DevedorCadastroSerialzer)
-@api_view(['GET', 'POST'])
-def devedor_list_2(request):
-    if request.method == 'GET':
-        devedores = Devedor.objects.all()
-        serializer = DevedorCadastroSerialzer(devedores, many=True)
-        return Response(serializer.data)
+# Classe para obter, atualizar e deletar um devedor específico
+class ExampleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Devedor.objects.all()
+    serializer_class = DevedorCadastroSerialzer
+    lookup_field = 'devedor_id'  # Especifica o campo 'devedor_id' para busca
 
-    elif request.method == 'POST':
-        serializer = DevedorCadastroSerialzer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @csrf_exempt  # Para desativar a proteção CSRF (não recomendado em produção)
+# @swagger_auto_schema(method='POST', request_body=DevedorCadastroSerialzer) #para sinalizar para o swagger que função mapeia métodos post e qual parametro o body precisa para criação
+# @api_view(['GET', 'POST'])
+# def devedor_list_2(request):
+#     if request.method == 'GET':
+#         devedores = Devedor.objects.all()
+#         serializer = DevedorCadastroSerialzer(devedores, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer = DevedorCadastroSerialzer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @csrf_exempt
+# @swagger_auto_schema(method='put', request_body=DevedorCadastroSerialzer)
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def devedor_detail_2(request, id):
+#     try:
+#         devedor = Devedor.objects.get(devedor_id=id)
+#     except Devedor.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+    
+#     if request.method == 'GET':
+#         serializer = DevedorCadastroSerialzer(devedor)
+#         return  Response(serializer.data, status=status.HTTP_200_OK)
+
+#     elif request.method == 'PUT':
+#         serializer = DevedorCadastroSerialzer(devedor, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'DELETE':
+#         devedor.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
